ba19a7086684ae0dd3db9c67f2411f3282b5c9ef
Jeroen Dries <jeroen.dries@vito.be>
2025-10-22 15:45:26 +0200

    add radiation downscaling

--- src/meteo_downscaling_openeo/run_agera_downscaler.py
+++ src/meteo_downscaling_openeo/run_agera_downscaler.py
@@ -1,11 +1,11 @@
 import openeo
 
-from .downscale_variables import downscale_shortwave_radiation, downscale_temperature_humidity
+from meteo_downscaling_openeo.downscale_variables import downscale_shortwave_radiation, downscale_temperature_humidity
 
 
 def run(spatial_extent):
 
-    c = openeo.connect("openeo-dev.vito.be").authenticate_oidc()
+    c = openeo.connect("openeo.cloud").authenticate_oidc()
 
     temporal_extent = ["2024-07-01","2024-07-05"]
     dem = c.load_collection("COPERNICUS_30", spatial_extent=spatial_extent)
@@ -15,8 +15,21 @@ def run(spatial_extent):
     geopotential.result_node().update_arguments(featureflags={"tilesize": 1})
     geopotential.metadata = geopotential.metadata.add_dimension("t", label="2025-09-29", type="temporal")
 
-    downscale_temperature_humidity(agera, dem, geopotential.max_time()).execute_batch( job_options={"executor-memory": "6G", "load_stac_apply_lcfm_improvements":True})
+    downscale_temperature_humidity(agera, dem, geopotential.max_time()).execute_batch( title="SNOWCOP Downscaling", job_options={"executor-memory": "6G", "load_stac_apply_lcfm_improvements":True})
 
-    #shortwave_rad_cube = downscale_shortwave_radiation(agera, None)
-    #shortwave_rad_cube.execute_batch(format="netCDF",filename_prefix="shortwave_radiation_")
+
+    aspect = c.load_stac(
+        "https://stac.openeo.vito.be/collections/DEM_aspec_30m",
+        spatial_extent=spatial_extent
+    ).reduce_dimension(dimension='t', reducer='mean')
+
+    slope = c.load_stac(
+        "https://stac.openeo.vito.be/collections/DEM_slope_30m",
+        spatial_extent=spatial_extent
+    ).reduce_dimension(dimension='t', reducer='mean')
+
+    slope_aspect = aspect.merge_cubes(slope).rename_labels(dimension="bands", target=["aspect", "slope"])
+
+    shortwave_rad_cube = downscale_shortwave_radiation(agera, slope_aspect)
+    shortwave_rad_cube.execute_batch(title="SNOWCOP Downscaling radiation", format="netCDF",filename_prefix="shortwave_radiation_")
 
