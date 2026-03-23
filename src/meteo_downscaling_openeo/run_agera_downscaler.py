09849cd7e13559544874937ead9cb6d186fe6056
Jeroen Dries <jeroen.dries@vito.be>
2026-03-23 07:46:14 +0100

    add temporal extent to udp & job opts

--- src/meteo_downscaling_openeo/run_agera_downscaler.py
+++ src/meteo_downscaling_openeo/run_agera_downscaler.py
@@ -10,11 +10,11 @@ def run(spatial_extent):
     shortwave_rad_cube = downscaled_temperature_humidity_radiation_cube(c, spatial_extent)
     shortwave_rad_cube.execute_batch(title="SNOWCOP Downscaling radiation", format="netCDF",filename_prefix="shortwave_radiation_",
                                                                                       job_options={
-                                                                                          "executor-memory": "10G"})
+                                                                                          "executor-memory": "8G"})
 
 
-def downscaled_temperature_humidity_radiation_cube(c, spatial_extent):
-    temporal_extent = ["2024-07-01", "2024-07-10"]
+def downscaled_temperature_humidity_radiation_cube(c, spatial_extent, temporal_extent = ["2024-07-01", "2024-07-10"]):
+
     dem = c.load_collection("COPERNICUS_30", spatial_extent=spatial_extent)
     agera = c.load_collection("AGERA5", spatial_extent=spatial_extent, temporal_extent=temporal_extent,
                               bands=["temperature-mean", "dewpoint-temperature", "solar-radiation-flux"])
